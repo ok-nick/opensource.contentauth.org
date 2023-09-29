7b237779d12c13745a58ec396a7c62be64f0084e
Rand McKinney <rmckinne@adobe.com>
2023-09-28 21:58:51 -0700

    Add redirect page

---
+++ src/pages/docs.js
@@ -0,0 +1,11 @@
+// Redirects /docs route to /docs/introduction
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
+
+const Home = () => {
+  return <Redirect to="/docs/introduction" />;
+};
+
+<Redirect to="/docs/introduction" />;
+
+export default Home;
