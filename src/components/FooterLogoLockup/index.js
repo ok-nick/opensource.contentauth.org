0c679b838f3b115f86548a289ff8f4d3fee324ed
Rand McKinney <crandmck@yahoo.com>
2025-01-24 14:57:18 -0800

    Remove unused assets and component scripts and css

--- src/components/FooterLogoLockup/index.js
+++ src/components/FooterLogoLockup/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
-import CAILogo from '../../assets/images/logo-cai.svg';
-import AdobeLogo from '../../assets/images/logo-adobe.svg';
-import XIcon from '../../assets/images/x.svg';
+//import CAILogo from '../../assets/images/logo-cai.svg';
+//import AdobeLogo from '../../assets/images/logo-adobe.svg';
+//import XIcon from '../../assets/images/x.svg';
 import styles from './styles.module.css';
 
 export default function FooterLogoLockup() {
   return (
     <div className={styles.root}>
-      <a href="https://contentauthenticity.org/" target="_blank">
-        <CAILogo className={styles.logoCAI} />
-      </a>
-      <XIcon className={styles.x} />
-      <a href="https://www.adobe.com/" target="_blank">
-        <AdobeLogo className={styles.logoAdobe} />
-      </a>
+      <a href="https://contentauthenticity.org/" target="_blank"></a>
+
+      <a href="https://www.adobe.com/" target="_blank"></a>
     </div>
   );
 }
